810d846d5729bca536823f00fb9f708082cfe44b
phudnguyen <phudnguyen@kms-technology.com>
2021-03-08 22:50:57 +0700

    update program rust

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -1,4 +1,6 @@
 use byteorder::{ByteOrder, LittleEndian};
+use arrayref::array_mut_ref;
+use num_derive::FromPrimitive;
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -8,8 +10,66 @@ use solana_program::{
     pubkey::Pubkey,
 };
 use std::mem;
-
+use thiserror::Error;
 use std::str::from_utf8;
+
+
+#[derive(Clone, Debug, Eq, Error, FromPrimitive, PartialEq)]
+pub enum MessageFeedError {
+    #[error("User is banned")]
+    BannedUser,
+    #[error("Next message already exists")]
+    NextMessageExists,
+    #[error("Creator mismatch")]
+    CreatorMismatch,
+}
+impl From<MessageFeedError> for ProgramError {
+    fn from(e: MessageFeedError) -> Self {
+        ProgramError::CustomError(e as u32)
+    }
+}
+impl<T> DecodeError<T> for MessageFeedError {
+    fn type_of() -> &'static str {
+        "MessageFeedError"
+    }
+}
+
+type PubkeyData = [u8; 32];
+
+struct UserAccountData<'a> {
+    pub banned: &'a mut bool,
+    pub creator: &'a mut PubkeyData,
+}
+impl<'a> UserAccountData<'a> {
+    fn new(data: &'a mut [u8]) -> Self {
+        let (banned, creator) = data.split_at_mut(1);
+        Self {
+            banned: unsafe { &mut *(&mut banned[0] as *mut u8 as *mut bool) },
+            creator: array_mut_ref!(creator, 0, size_of::<PubkeyData>()),
+        }
+    }
+}
+
+struct MessageAccountData<'a> {
+    pub next_message: &'a mut PubkeyData,
+    pub from: &'a mut PubkeyData,
+    pub creator: &'a mut PubkeyData,
+    pub text: &'a mut [u8],
+}
+impl<'a> MessageAccountData<'a> {
+    fn new(data: &'a mut [u8]) -> Self {
+        let (next_message, rest) = data.split_at_mut(size_of::<PubkeyData>());
+        let (from, rest) = rest.split_at_mut(size_of::<PubkeyData>());
+        let (creator, text) = rest.split_at_mut(size_of::<PubkeyData>());
+        Self {
+            next_message: array_mut_ref!(next_message, 0, size_of::<PubkeyData>()),
+            from: array_mut_ref!(from, 0, size_of::<PubkeyData>()),
+            creator: array_mut_ref!(creator, 0, size_of::<PubkeyData>()),
+            text,
+        }
+    }
+}
+
 
 // Declare and export the program's entrypoint
 entrypoint!(process_instruction);
