592cebf508601c09e54d4b5a05f130c858e34ab3
phudnguyen <phudnguyen@kms-technology.com>
2021-03-08 23:11:01 +0700

    update program rust

--- src/program-rust/src/lib.rs
+++ src/program-rust/src/lib.rs
@@ -83,22 +83,30 @@ fn process_instruction(
 ) -> ProgramResult {
     msg!("Helloworld Rust program entrypoint");
 
-    // Iterating accounts is safer then indexing
-    let accounts_iter = &mut accounts.iter();
+    let account_info_iter = &mut accounts.iter();
 
-    // Get the account to say hello to
-    let account = next_account_info(accounts_iter)?;
+    let user_account = next_account_info(account_info_iter)?;
+    let mut user_data = user_account.data.borrow_mut();
+    let user_data = UserAccountData::new(&mut user_data);
 
-    // The account must be owned by the program in order to modify its data
-    if account.owner != program_id {
-        msg!("Greeted account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
+    let message_account = next_account_info(account_info_iter)?;
+    let mut new_message_data = message_account.data.borrow_mut();
+    let new_message_data = MessageAccountData::new(&mut new_message_data);
+
+    if !user_account.is_signer {
+        msg!("Error: not signed by key 0");
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+    if !message_account.is_signer {
+        msg!("Error: not signed by key 1");
+        return Err(ProgramError::MissingRequiredSignature);
     }
 
-    // The data must be large enough to hold a u32 count
-    if account.try_data_len()? < size_of::<u32>() {
-        msg!("Greeted account data length too small for u32");
-        return Err(ProgramError::InvalidAccountData);
+    if input.is_empty() {
+        msg!("Creating user...");
+        user_data
+            .creator
+            .clone_from_slice(message_account.key.as_ref());
     }
 
     let mesg = from_utf8(input).map_err(|err| {
@@ -107,13 +115,7 @@ fn process_instruction(
     })?;
     msg!("Received message (len {}): {:?}", mesg.len(), mesg);
 
-    // Increment and store the number of times the account has been greeted
-    let mut data = account.try_borrow_mut_data()?;
-    let mut num_greets = LittleEndian::read_u32(&data);
-    num_greets += 1;
-    LittleEndian::write_u32(&mut data[0..], num_greets);
-
-    msg!("Hello!");
+    msg!("Success");
 
     Ok(())
 }
